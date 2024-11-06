67478814dee39fe3d81fb626bb80f2d06509825b
anonfedora <musaeleazar090@gmail.com>
2024-11-06 11:57:05 +0100

    feat: say_name function

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 fn main() {
     gm_to_rust();
+    say_name("Eleazar");
 }
 
 // function to print GM Rust!
 fn gm_to_rust() {
     println!("GM Rust!");
+}
+
+fn say_name(name: &str) {
+    println!("{}", name);
 }
